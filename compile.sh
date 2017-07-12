ce13361004f9684ae8ba02cfab28f4af5b9ce403
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2017-07-12 14:47:09 -0700

    Agregar botones, y probar colores.

--- compile.sh
+++ compile.sh
@@ -3,11 +3,11 @@ echo 'Compiling all, please wait...'
 
 sass src/all.scss:dist/all.css
 
-sass src/content.scss:dist/content.css
-styledown dist/content.css config.md > guide/content.html
-
 sass src/layout.scss:dist/layout.css
 styledown dist/layout.css config.md > guide/layout.html
 
-sass src/utilities.scss:dist/utilities.css
-styledown dist/utilities.css config.md > guide/utilities.html
+sass src/elements.scss:dist/elements.css
+styledown dist/elements.css config.md > guide/elements.html
+
+sass src/helpers.scss:dist/helpers.css
+styledown dist/helpers.css config.md > guide/helpers.html
